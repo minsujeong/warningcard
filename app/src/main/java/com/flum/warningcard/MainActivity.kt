4a2f0f47b819c12ec9fc644a8b5fcc3d7538bead
minsujeong <regako@hanmail.net>
2024-02-11 17:39:42 +0900

    카드 첫구현 완료

--- app/src/main/java/com/flum/warningcard/MainActivity.kt
+++ app/src/main/java/com/flum/warningcard/MainActivity.kt
@@ -1,46 +1,70 @@
 package com.flum.warningcard
 
+import android.content.SharedPreferences
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import com.flum.warningcard.ui.theme.RedWarning
 import com.flum.warningcard.ui.theme.WarningCardTheme
+import com.flum.warningcard.ui.theme.YellowWarning
+
 
 class MainActivity : ComponentActivity() {
+
+    companion object {
+        const val WARNING_INTERVAL_MINUTE = 1
+    }
+
+    lateinit var sp : SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowInsetsControllerCompat(window, window.decorView).apply {
+            hide(WindowInsetsCompat.Type.systemBars())
+            systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+
+        sp = getSharedPreferences("time", MODE_PRIVATE)
+
         setContent {
-            WarningCardTheme {
-                // A surface container using the 'background' color from the theme
+            WarningCardTheme(darkTheme = false) {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color.White
                 ) {
-                    Greeting("Android")
+                    val cardColor = if(checkRedCard()) RedWarning else YellowWarning
+                    Box(modifier = Modifier
+                        .padding(5.dp)
+                        .fillMaxSize()
+                        .clip(shape = RoundedCornerShape(10))
+                        .background(cardColor)) {
+                    }
+                    sp.edit().putLong("time", System.currentTimeMillis()).apply()
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WarningCardTheme {
-        Greeting("Android")
+    private fun checkRedCard() : Boolean {
+        val showTime = sp.getLong("time", 0)
+        if(showTime == 0L) {
+            return false
+        }
+        return System.currentTimeMillis() - showTime < (WARNING_INTERVAL_MINUTE * 60 * 1000)
     }
 }
--- app/src/main/java/com/flum/warningcard/ui/theme/Color.kt
+++ app/src/main/java/com/flum/warningcard/ui/theme/Color.kt
@@ -9,3 +9,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val YellowWarning = Color(0xFFFDE731)
+val RedWarning = Color(0xFFFC260E)
